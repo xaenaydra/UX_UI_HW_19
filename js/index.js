e1f9ca8a02b057a43e15b33945bc5429f2c5c4dd
xaenaydra <xaenaydra@gmail.com>
2024-02-15 12:23:05 -0800

    19.2

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function changeImage(x,image)
+{
+    if(x==1)
+    {
+        image.src ="images/Profile Pic.png"
+    }
+    if(x==2)
+    {
+        image.src = "images/Logo Round.png"
+    }
+}
